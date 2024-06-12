b95ebf14498f39ddc4326361dbaa32bbc907d80e
timothy ademola <timife007@gmail.com>
2024-06-12 21:53:32 +0100

    transfer funds ui completed

---
+++ app/src/main/java/com/timife/vpdmoneyassessment/presentation/transfer/TransferViewModel.kt
@@ -0,0 +1,7 @@
+package com.timife.vpdmoneyassessment.presentation.transfer
+
+import androidx.lifecycle.ViewModel
+
+class TransferViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
