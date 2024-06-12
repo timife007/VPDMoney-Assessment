73d24bd57df83419f0bddfc495019ec8a8dc917d
timothy ademola <timife007@gmail.com>
2024-06-12 23:55:48 +0100

    dashboard ui implemented

---
+++ app/src/main/java/com/timife/vpdmoneyassessment/presentation/dashboard/DashboardViewModel.kt
@@ -0,0 +1,7 @@
+package com.timife.vpdmoneyassessment.presentation.dashboard
+
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
